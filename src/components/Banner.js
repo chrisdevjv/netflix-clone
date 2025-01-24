be95ced153b411b5eadf09e199937753900f4672
chris-20mb <christophermachado222@gmail.com>
2025-01-24 17:03:52 -0300

    "construindo o filme em destaque"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -21,12 +21,34 @@ function Banner() {
     useEffect(() => {
         fetchRandomMovie()
     }, [])
+
+    function truncate(str, n) {
+        return str?.length > n ? str.subtr(0, n - 1) + "..." : str;
+    }
     
   return <header className='banner-container' style={{
      backgroundSize: "cover",
      backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
      groundPosition: "center-center",
-  }}> </header>
+  }}> 
+  <div className='banner-content'>
+  <h1 className='banner-title'>
+        {movie?.title || movie?.name || movie?.original_name}
+  </h1>
+  <div className='banner-button-container'>
+    <button className='banner-button'>
+            Assistir
+    </button>
+    <button className='banner-button'>
+            Minha Lista
+    </button>
+  </div>
+  
+  <div className='banner-description'>
+        <h2>{truncate(movie?.overview)}</h2>
+  </div>
+  </div>
+  </header>
 
 }
 
